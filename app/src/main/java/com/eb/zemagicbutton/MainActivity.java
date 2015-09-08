c5a8c741b2e3a97af70d9976efcd17391f7e5570
Manu <ebeloin@hotmail.com>
2015-09-07 22:50:52 -0400

    t5t4

--- app/src/main/java/com/eb/zemagicbutton/MainActivity.java
+++ app/src/main/java/com/eb/zemagicbutton/MainActivity.java
@@ -4,8 +4,167 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int CLIQUETOTAL = 10;
+    int nbClique = 0;
+    int lastButtonCliqued = 0;
+    int lastRandom = 0;
+    Random rand = new Random();
+
+    public void DemarrerPartie(View view) {
+        View b = findViewById(R.id.BTN_Start);
+        b.setVisibility(View.INVISIBLE);
+        int random = rand.nextInt(16) + 1;
+        changerVisibiliteBouton(random);
+        lastRandom = random;
+    }
+
+    public void cliquerBouton(View view) {
+        findViewById(lastButtonCliqued).setVisibility(View.INVISIBLE);
+        int random;
+        do {
+            random = rand.nextInt(16) + 1;
+            lastRandom = random;
+        }while(random == lastRandom);
+        changerVisibiliteBouton(random);
+        nbClique++;
+        verifierFinPartie(view, view.getId());
+    }
+
+
+    public void changerVisibilite(int BoutonID) {
+        switch (BoutonID) {
+            case R.id.BTN_1:
+                findViewById(R.id.BTN_1).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_2:
+                findViewById(R.id.BTN_2).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_3:
+                findViewById(R.id.BTN_3).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_4:
+                findViewById(R.id.BTN_4).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_5:
+                findViewById(R.id.BTN_5).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_6:
+                findViewById(R.id.BTN_6).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_7:
+                findViewById(R.id.BTN_7).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_8:
+                findViewById(R.id.BTN_8).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_9:
+                findViewById(R.id.BTN_9).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_10:
+                findViewById(R.id.BTN_10).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_11:
+                findViewById(R.id.BTN_11).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_12:
+                findViewById(R.id.BTN_12).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_13:
+                findViewById(R.id.BTN_13).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_14:
+                findViewById(R.id.BTN_14).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_15:
+                findViewById(R.id.BTN_15).setVisibility(View.VISIBLE);
+                break;
+            case R.id.BTN_16:
+                findViewById(R.id.BTN_16).setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
+    public void changerVisibiliteBouton(int BoutonID) {
+        switch (BoutonID) {
+            case 1:
+                findViewById(R.id.BTN_1).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_1;
+                break;
+            case 2:
+                findViewById(R.id.BTN_2).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_2;
+                break;
+            case 3:
+                findViewById(R.id.BTN_3).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_3;
+                break;
+            case 4:
+                findViewById(R.id.BTN_4).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_4;
+                break;
+            case 5:
+                findViewById(R.id.BTN_5).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_5;
+                break;
+            case 6:
+                findViewById(R.id.BTN_6).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_6;
+                break;
+            case 7:
+                findViewById(R.id.BTN_7).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_7;
+                break;
+            case 8:
+                findViewById(R.id.BTN_8).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_8;
+                break;
+            case 9:
+                findViewById(R.id.BTN_9).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_9;
+                break;
+            case 10:
+                findViewById(R.id.BTN_10).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_10;
+                break;
+            case 11:
+                findViewById(R.id.BTN_11).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_11;
+                break;
+            case 12:
+                findViewById(R.id.BTN_12).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_12;
+                break;
+            case 13:
+                findViewById(R.id.BTN_13).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_13;
+                break;
+            case 14:
+                findViewById(R.id.BTN_14).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_14;
+                break;
+            case 15:
+                findViewById(R.id.BTN_15).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_15;
+                break;
+            case 16:
+                findViewById(R.id.BTN_16).setVisibility(View.VISIBLE);
+                lastButtonCliqued = R.id.BTN_16;
+                break;
+        }
+    }
+
+    public void verifierFinPartie(View view, int boutonID) {
+        if (nbClique == CLIQUETOTAL) {
+            findViewById(boutonID).setVisibility(View.INVISIBLE);
+            findViewById(R.id.BTN_Start).setVisibility(View.VISIBLE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
